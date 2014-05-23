e6944e0238969a683754f535e8e910a27d575d7b
Aisa <aisapatino@gmail.com>
2014-05-23 10:11:46 -0700

    remove unneeded code

--- app/js/app.js
+++ app/js/app.js
@@ -2,16 +2,9 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 var Router = require('./router.js');
 
-var Place = require('./models/Place.js');
-var PlaceView = require('./views/PlaceView.js');
-
-var Places = require('./collections/Places.js');
-var PlacesView = require('./views/PlacesView.js');
-
-var Along = Along || {};
-
 var router = new Router();
 $(function(){
   Backbone.history.start();
   Backbone.history.navigate('');
 });
+
