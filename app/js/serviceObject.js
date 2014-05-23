da0f099b77591aff5cb2aa28b04186f89e3c378f
Josh Cunningham <josh.cunningham@10up.com>
2014-05-23 12:57:48 -0700

    starting serviceobject part

---
+++ app/js/serviceObject.js
@@ -0,0 +1,10 @@
+/* globals require, module */
+var Backbone = require('backbone');
+var $ = require('jquery');
+Backbone.$ = $;
+
+var ServiceObject = {
+	locations: []
+};
+
+module.exports = ServiceObject;
