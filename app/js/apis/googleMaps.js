63043714dd8f60c2db25dfb1d181d14fa166447a
Aisa <aisapatino@gmail.com>
2014-05-27 14:24:42 -0700

    clear old route/markers before getting new one #23

--- app/js/apis/googleMaps.js
+++ app/js/apis/googleMaps.js
@@ -8,37 +8,15 @@ var googleMapServices = {
   // define object properties that are set later
   map: null,
   placesService: null,
+  DirectionsService: null,
+  directionsDisplay: null,
   geocoder: null,
   routeBoxer: new RouteBoxer(),
-
-  // types:[] to search for when returning places
-  // see https://developers.google.com/places/documentation/supported_types
-  filter: {
-    entertainment: ['amusement_park', 'aquarium', 'art_gallery',
-    'bowling_alley', 'casino', 'movie_rental', 'movie_theater',
-    'stadium', 'museum', 'night_club', 'park','zoo'],
-    stores: ['bicycle_store', 'book_store', 'clothing_store', 'convenience_store',
-    'department_store', 'electronics_store', 'home_goods_store', 'jewelry_store',
-    'liquor_store', 'hardware_store', 'store', 'shoe_store', 'shopping_mall',
-    'pet_store', 'grocery_or_supermarket', 'florist'],
-    services: ['car_repair', 'car_wash', 'gas_station', 'laundry'],
-    food: ['bakery', 'bar', 'cafe', 'food', 'meal_delivery',
-    'meal_takeaway', 'restaurant'],
-    aesthetics: ['beauty_salon', 'gym', 'hair_care', 'spa'],
-    transportation: ['bus_station', 'subway_station', 'taxi_stand', 'train_station'],
-    banking: ['atm', 'bank', 'post_office'],
-    education: ['school', 'university', 'library']
-  },
-
-  filterFunc: function filterFunc(checked){
-    var finalFilter = [];
-    for(var i=0; i < checked.length; i++){
-      finalFilter = finalFilter.concat(this.filter[checked[i]]);
-    }
-    console.log('filterfunc call: '+ finalFilter);
-    this.buildRoute(null, finalFilter);
-
-  },
+  markers: [],
+  infoWindow: null,
+  start: null,
+  end: null,
+  placeTypes: null,
 
   /* Set up maps services */
   initialize: function initialize(geoposition) {
@@ -93,7 +71,7 @@ var googleMapServices = {
       });
   },
 
-  /* Receive geocoded start or end point. */
+  /* Receive start, end, and type filters.  Wait for all 3 to proceed */
   buildRoute: function(location, placeTypes) {
     if (placeTypes) {
       this.placeTypes = placeTypes;
@@ -105,16 +83,24 @@ var googleMapServices = {
       }
     }
     if (this.start && this.end && this.placeTypes) {
+      if (this.directionsDisplay) {
+        this.clearAllMarkers();
+        this.directionsDisplay.setMap(null);
+      }
       this.getDirections(this.start, this.end);
+      this.start = null;
+      this.end = null;
+      this.placeTypes = null;
     }
   },
 
   getDirections: function getDirections(start, end) {
     var startLL = new google.maps.LatLng(start.get('lat'), start.get('lng'));
     var endLL = new google.maps.LatLng(end.get('lat'), end.get('lng'));
-    this.directionsService = this.directionService || new google.maps.DirectionsService()
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(this.map);
+    this.directionsService = this.directionsService || new google.maps.DirectionsService();
+    this.directionsDisplay = this.directionsDisplay || new google.maps.DirectionsRenderer();
+    this.directionsDisplay.setMap(this.map);
+    //this.directionsDisplay.setDirections({routes: []});
 
     var opts = {
       origin: startLL,
@@ -125,7 +111,7 @@ var googleMapServices = {
     var that = this;
     this.directionsService.route(opts, function(result, status) {
       if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(result);
+        that.directionsDisplay.setDirections(result);
         that.createRouteBoxes(result.routes[0].overview_path);
       } else {
         console.warn(status);
@@ -136,7 +122,7 @@ var googleMapServices = {
 
   /* creates LatLngBounds that cover the entire route and can be used in nearbySeach */
   createRouteBoxes: function(overview_path) {
-    var boxes = this.routeBoxer.box(overview_path, 1);
+    var boxes = this.routeBoxer.box(overview_path, .125);
     // show boxes on map (for devel)
     var rectangleOpts = {
       strokeColor: '#000',
@@ -155,59 +141,86 @@ var googleMapServices = {
   },
 
   getNearbyPlaces: function getNearbyPlaces(latLngBounds) {
-    console.log('getNearbyPlaces');
-    console.log(this.placeTypes);
-    console.log(['bank', 'atm']);
-    var that = this;
     var opts = {
       bounds: latLngBounds,
-      types: that.placeTypes
     }
+    if (this.placeTypes && this.placeTypes !== []) {
+      opts.types = this.placeTypes
+    }
+
+    var that = this;
     this.placesService.nearbySearch(opts, function(results, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
-        that.putPlacesOnMap(0, results);
-          /*
-          var place = new PlaceModel({
-            name: results[i].name,
-            lat: results[i].geometry.location.lat(),
-            lng: results[i].geometry.location.lng(),
-            types: results[i].types,
-            rating: results[i].rating,
-            address: results[i].vicinity
+        // re-use the same infowindow for all markers
+        this.infoWindow = this.infoWindow || new google.maps.InfoWindow();
+        for (var i = 0; i < results.length; i++) {
+          var result = results[i];
+          var marker = new google.maps.Marker({
+            position: result.geometry.location,
+            map: that.map,
+            visible: true
           });
-          collection.add(place);
-          */
+          that.markers.push(marker);
+          var placeContent = template({
+              name: result.name,
+              rating: result.rating,
+              address: result.vicinity
+          });
+
+          google.maps.event.addListener(marker, 'click', function(result) {
+            this.infoWindow.setContent(template({
+              name: result.name,
+              rating: result.rating,
+              address: result.vicinity
+            }));
+            this.infoWindow.open(that.map, marker);
+          }(result, that));
+        }
       } else {
         console.log('ERROR: ' + status);
       }
     }, this);
   },
 
-  putPlacesOnMap: function(i, results) {
-    if (i >= results.length) {
-      return false;
+  // types:[] to search for when returning places
+  // see https://developers.google.com/places/documentation/supported_types
+  filter: {
+    entertainment: ['amusement_park', 'aquarium', 'art_gallery',
+    'bowling_alley', 'casino', 'movie_rental', 'movie_theater',
+    'stadium', 'museum', 'night_club', 'park','zoo'],
+    stores: ['bicycle_store', 'book_store', 'clothing_store', 'convenience_store',
+    'department_store', 'electronics_store', 'home_goods_store', 'jewelry_store',
+    'liquor_store', 'hardware_store', 'store', 'shoe_store', 'shopping_mall',
+    'pet_store', 'grocery_or_supermarket', 'florist'],
+    services: ['car_repair', 'car_wash', 'gas_station', 'laundry'],
+    food: ['bakery', 'bar', 'cafe', 'food', 'meal_delivery',
+    'meal_takeaway', 'restaurant'],
+    aesthetics: ['beauty_salon', 'gym', 'hair_care', 'spa'],
+    transportation: ['bus_station', 'subway_station', 'taxi_stand', 'train_station'],
+    banking: ['atm', 'bank', 'post_office'],
+    education: ['school', 'university', 'library']
+  },
+
+  filterFunc: function filterFunc(checked){
+    var finalFilter = [];
+    for(var i=0; i < checked.length; i++){
+      finalFilter = finalFilter.concat(this.filter[checked[i]]);
     }
-    var result = results[i];
-    var marker = new google.maps.Marker({
-      position: result.geometry.location,
-      map: this.map,
-      visible: true
-    });
-    var placeContent = template({
-        name: result.name,
-        rating: result.rating,
-        address: result.vicinity
-    });
-    marker.info = new google.maps.InfoWindow({
-      content: placeContent
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-      marker.info.open(this.map, marker);
-    });
-    this.putPlacesOnMap(i+1, results);
+    this.buildRoute(null, finalFilter);
+
+  },
+  
+  clearAllMarkers: function() {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(null);
+    }
+    this.markers.length = 0;
   }
+
+
 
 }
 
+global.GOOOGZ = googleMapServices;
 module.exports = googleMapServices;
 
