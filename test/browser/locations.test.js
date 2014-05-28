2d13dca7314d3d95f8b1cd50de65514b66427152
Aisa <aisapatino@gmail.com>
2014-05-27 17:33:49 -0700

    expect type instead of just truthy

--- test/browser/locations.test.js
+++ test/browser/locations.test.js
@@ -4,66 +4,66 @@ var googleMapServices = require('../../app/js/apis/googleMaps.js');
 
 describe('Location', function () {
 
-	var location1, location2;
+  var location1, location2;
 
-	before(function () {
+  before(function () {
 
-		location1 = new Location({
-			search: 'Seattle, WA',
-			order: 0
-		});
+    location1 = new Location({
+      search: 'Seattle, WA',
+      order: 0
+    });
 
-		location2 = new Location({
-			search: 'Portland, OR',
-			order : 1
-		});
+    location2 = new Location({
+      search: 'Portland, OR',
+      order : 1
+    });
 
-	});
+  });
 
-	describe('model', function () {
+  describe('model', function () {
 
-		this.timeout(4000);
+    this.timeout(4000);
 
-		it('#1 and #2 should both exist', function (done) {
-			expect(location1).to.be.ok;
-			expect(location2).to.be.ok;
-			setTimeout(done, 3000);
-		});
+    it('#1 and #2 should exist and be objects', function (done) {
+      expect(location1).to.be.an('object');
+      expect(location2).to.be.an('object');
+      setTimeout(done, 3000);
+    });
 
-		it('#1 should have a valid latitude', function () {
-//			expect(location1.get('lat')).to.be.ok;
-//			expect(location1.get('lat')).to.be.at.least(-90);
-//			expect(location1.get('lat')).to.be.at.most(90);
-		});
+    it('#1 should have a valid latitude', function () {
+      expect(location1.get('lat')).to.be.a('number');
+      expect(location1.get('lat')).to.be.at.least(-90);
+      expect(location1.get('lat')).to.be.at.most(90);
+    });
 
-		it('#1 should have a valid longitude', function () {
-//			expect(location1.get('lng')).to.be.ok;
-//			expect(location1.get('lng')).to.be.at.least(-180);
-//			expect(location1.get('lng')).to.be.at.most(180);
-		});
+    it('#1 should have a valid longitude', function () {
+      expect(location1.get('lng')).to.be.a('number');
+      expect(location1.get('lng')).to.be.at.least(-180);
+      expect(location1.get('lng')).to.be.at.most(180);
+    });
 
-		it('#2 should have a valid latitude', function () {
-//			expect(location2.get('lat')).to.be.ok;
-//			expect(location2.get('lat')).to.be.at.least(-90);
-//			expect(location2.get('lat')).to.be.at.most(90);
-		});
+    it('#2 should have a valid latitude', function () {
+      expect(location2.get('lat')).to.be.a('number');
+      expect(location2.get('lat')).to.be.at.least(-90);
+      expect(location2.get('lat')).to.be.at.most(90);
+    });
 
-		it('#2 should have a valid longitude', function () {
-//			expect(location2.get('lng')).to.be.ok;
-//			expect(location2.get('lng')).to.be.at.least(-180);
-//			expect(location2.get('lng')).to.be.at.most(180);
-		});
+    it('#2 should have a valid longitude', function () {
+      expect(location2.get('lng')).to.be.a('number');
+      expect(location2.get('lng')).to.be.at.least(-180);
+      expect(location2.get('lng')).to.be.at.most(180);
+    });
 
-		it('should build a route', function () {
-			googleMapServices.getDirections(location1, location1);
-		});
+    it('should build a route', function () {
+      googleMapServices.getDirections(location1, location2);
+    });
 
-	});
+  });
 
-	describe('collection', function () {
+  describe('collection', function () {
 
-		it('should build a route');
+    it('should build a route');
 
-	});
+  });
 
 });
