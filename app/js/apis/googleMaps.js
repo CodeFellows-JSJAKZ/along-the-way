3d8c9e11b684834175efc0d64aff6821c511f370
Aisa <aisapatino@gmail.com>
2014-05-29 10:26:36 -0700

    prevent multiple infowindows from showing at once. fixes #47

--- app/js/apis/googleMaps.js
+++ app/js/apis/googleMaps.js
@@ -184,10 +184,10 @@ var googleMapServices = {
     });
   },
 
-
+  /* Given nearbySeach results, put marker on map for each */
   putPlacesOnMap: function putPlacesOnMap(results) {
     // re-use the same infowindow for all markers
-    this.infoWindow = new google.maps.InfoWindow();
+    this.infoWindow = this.infoWindow || new google.maps.InfoWindow();
 
     // set maximum places per routebox
     var maxPlaces = Math.min(5, results.length);
